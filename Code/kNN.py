8e67ff97a51ef4905bd4bb523480594100a7c0f9
Vishwanath Acharya <bailorevishwanath02@gmail.com>
2018-04-08 13:26:24 +0530

    Add proper comments and licence information.

--- Code/kNN.py
+++ Code/kNN.py
@@ -1,3 +1,25 @@
+'''
+This file is part of Classification of Large Atsronomical Photometric Datasets.
+
+Classification of Large Atsronomical Photometric Datasets is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Classification of Large Atsronomical Photometric Datasets is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+'''
+The goal of this script is to classify the Astronomical Datasets into Stars, Quasars and Galaxies
+using distributed kNN implementation in pySpark over Google Cloud framework
+'''
+
 from pyspark import SparkContext
 from pyspark.sql import SQLContext
 from pyspark.mllib.linalg import Vectors
@@ -12,17 +34,18 @@ import os
 import warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-#os.path.abspath('/media/vishwanath/Vishwanath/IIITB/Sem_2/PE/Data/Training_data.csv')
+
+# Create spark context and read files from the cloud storage
 sc = SparkContext("local", "test_script")
-sqlContext = SQLContext(sc)
 trainingFile = "gs://vishu/TrainingData/Training_Data.csv"
 data = sc.textFile(trainingFile).repartition(60)
 
-#Remove Header
-##header = data.first()
-##data = data.filter(lambda line: line != header)
-##print data.take(3)
+# Remove Header
+header = data.first()
+data = data.filter(lambda line: line != header)
 
+
+# Function to compute object features as g-r, u-r, r-i, i-z
 def transformToLabelledPoint(inputStr):
     attlist = inputStr.split(",")
     data = (float(attlist[2]) - float(attlist[4]),
@@ -31,62 +54,34 @@ def transformToLabelledPoint(inputStr):
          float(attlist[3]) - float(attlist[6]))
     return data
 
+# Function to transform class label to a numerical vector
 def tranformToClass(inputStr):
     attlist = inputStr.split(",")
     classes = (float(attlist[0]), attlist[1])
     return classes
 
+# Extract features from all the objects in a distributed framework
 autolp = data.map(transformToLabelledPoint)
 autolpCollect = autolp.collect()
-##autoDF = sqlContext.createDataFrame(autolp, ["g-r","u-r","r-i","i-z"])
 vectorsCollected = np.vstack(tuple(autolpCollect))
 
+# Transform class label to numerical vector in distributed framework
 classes = data.map(tranformToClass)
 classDF = sqlContext.createDataFrame(classes, ["id","class"])
 dfclass = classDF.toPandas()
 classPD = pd.Series(dfclass['class'])
 
+# Create kNN model and fit the model to the training dataset
 knn = KNeighborsClassifier(n_neighbors=7)
 knn.fit(vectorsCollected, classPD)
 
-##(trainingData, testData) = autoDF.randomSplit([0.8,0.2])
-##df = trainingData.toPandas()
-#print df
-##df2 = testData.toPandas()
-##nbrs = NearestNeighbors(n_neighbors=7, algorithm='auto').fit(vectorsCollected)
+# Create a broadcasted object for a knn model
 bc_knnobj = sc.broadcast(knn)
-##knnlist = []
-##bc_knnobj = sc.broadcast(knnlist)
 
-##distances, indices = nbrs.kneighbors(vectorsCollected)
-##results = autolp.map(lambda x: bc_knnobj.value.kneighbors(x))
-
+# Predict the classes for all the testing objects in a distributed framework and accumulate the results
+# into a RDD object
 results = autolp.map(lambda x: bc_knnobj.value.predict(x))
-print results.take(5)
 
-
+# Convert RDD object to a tuple and store the results in a text file in google cloud storage
 resultlist = results.map(lambda x: (x.item(0)))
 resultlist.saveAsTextFile("gs://vishu/Result")
-#resultlist = resultlist.zipWithIndex().collect()
-##df = resultlist.map(lambda x: Row(x)).toDF()
-#print resultlist.take(5)
-
-##predictedClassDF = sqlContext.createDataFrame(resultlist, ["id","class"])
-##predictedClassDF.select("id","class").show(5)
-
-
-##predicted_class = []
-##for i in range(indices.size/7):
-##    quasar_count = 0
-##    non_quasar_count = 0
-##    for j in range(indices[i].size):
-##        if(classes[indices[i,j]] == 'QSO'):
-##            quasar_count += (1/(distances[i,j]*distances[i,j]))
-##        else:
-##            non_quasar_count += (1/(distances[i,j]*distances[i,j]))
-##    if(quasar_count > non_quasar_count):
-##        predicted_class.append('QSO')
-##    else:
-##        predicted_class.append('NQSO')
-##print predicted_class
-##autoDF.printSchema()
