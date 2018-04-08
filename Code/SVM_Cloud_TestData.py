ed75f8eaad5e21add5b55e881f641ea682ff3ac0
Vishwanath Acharya <bailorevishwanath02@gmail.com>
2018-04-08 13:36:32 +0530

    Add Comments and license information.

--- Code/SVM_Cloud_TestData.py
+++ Code/SVM_Cloud_TestData.py
@@ -1,12 +1,36 @@
+'''
+This file is part of Classification of Large Atsronomical Photometric Datasets.
+
+Classification of Large Atsronomical Photometric Datasets is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Classification of Large Atsronomical Photometric Datasets is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+'''
+The goal of this script is to classify the Astronomical Datasets into Stars, Quasars and Galaxies
+using distributed SVM implementation in pySpark over Google Cloud framework
+'''
+
 from pyspark import SparkContext
 from pyspark.mllib.classification import SVMWithSGD, SVMModel
 from pyspark.mllib.regression import LabeledPoint
 from pyspark.mllib.evaluation import BinaryClassificationMetrics
 
+# Create spark context and read files from the cloud storage
 sc = SparkContext("local", "test_script")
 trainingFile = "gs://vishu/TrainingData/Training_Data.csv"
 data = sc.textFile(trainingFile)
 
+# Function to compute object features as g-r, u-r, r-i, i-z and map class label to numerical vector
 def parsePoint(line):
     attlist = line.split(",")
     label = 0
@@ -20,17 +44,20 @@ def parsePoint(line):
                 float(attlist[3]) - float(attlist[6])]
     return LabeledPoint(label, features)
 
-#Remove Header
+# Remove Header and extract the features from the data using distributed framework
 header = data.first()
 data = data.filter(lambda line: line != header)
 parsedData = data.map(parsePoint)
 
 
+# Create a SVM model and train the model using training dataset
 model = SVMWithSGD.train(parsedData, iterations=100)
 
+# Read the testing file
 testingFile = "gs://vishu/TestingData/"
 testData = sc.textFile(testingFile)
 
+# Function to obtain features from the testing file as g-r, u-r, r-i, i-z
 def parseTestData(line):
     attlist = line.split(",")
     label = int(attlist[0])
@@ -40,10 +67,13 @@ def parseTestData(line):
                 float(attlist[5]) - float(attlist[8])]
     return LabeledPoint(label, features)
 
+# Parse the testing file using a distributed framework
 parsedTestData = testData.map(parseTestData)
 
+# Using the parsed data to predict the classes in a distributed framework
 labelsAndPreds = parsedTestData.map(lambda p: (p.label, model.predict(p.features)))
 
+# Function to map numerical vector to class labels
 def getResults(t):
     result = ""
     if t[1] == 0:
@@ -52,6 +82,6 @@ def getResults(t):
         return (int(t[0]),"QSO")
 
 
+# Parse the results and store it in a cloud storage
 resultList = labelsAndPreds.map(getResults)
 resultList.saveAsTextFile("gs://vishu/SVMTestResult")
-print resultList.take(5)
